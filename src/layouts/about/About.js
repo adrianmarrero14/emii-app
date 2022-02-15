32c6094de2296d198c72c2441baf24946130836b
Adrian <adrianmarrero18@gmail.com>
2022-02-15 12:34:27 -0400

    react router <Link> used

--- src/layouts/about/About.js
+++ src/layouts/about/About.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class About extends React.Component {
     render(){
-        return <div>About</div>
+        return (
+            <>
+                <div>About</div>
+                <Link to="/">Home</Link>
+            </>
+        )
     }
 }
 
--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
     render(){
-        return <div>Home</div>
+        return (
+            <>
+                <div>Home</div>
+                <Link to="/about">About</Link>
+            </>
+        )
     }
 }
 
